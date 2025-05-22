b8797b751e29e0a666feebff13557cbc13792897
Ramus ASSOGBA <ramus@sharemat.fr>
2025-05-22 20:48:18 +0200

    feat: add videos

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const slider = document.getElementById("timelineSlider")
+const slider = document.getElementById("timelineSlider");
 document.getElementById("scrollLeft").onclick = () => {
-  slider.scrollBy({ left: -300, behavior: "smooth" })
-}
+  slider.scrollBy({ left: -400, behavior: "smooth" });
+};
 document.getElementById("scrollRight").onclick = () => {
-  slider.scrollBy({ left: 300, behavior: "smooth" })
-}
+  slider.scrollBy({ left: 400, behavior: "smooth" });
+};
 
-const looksSlider = document.getElementById("looksSlider")
+const looksSlider = document.getElementById("looksSlider");
 
 document.getElementById("lookScrollLeft").onclick = () => {
-  looksSlider.scrollBy({ left: -300, behavior: "smooth" })
-}
+  looksSlider.scrollBy({ left: -400, behavior: "smooth" });
+};
 document.getElementById("lookScrollRight").onclick = () => {
-  looksSlider.scrollBy({ left: 300, behavior: "smooth" })
-}
+  looksSlider.scrollBy({ left: 400, behavior: "smooth" });
+};
+
+const burger = document.getElementById("burger");
+const navLinks = document.getElementsByClassName("nav-links")[0];
+const listItems = navLinks.querySelectorAll("li");
+
+burger.onclick = () => {
+  navLinks.classList.toggle("active");
+};
+
+listItems.forEach((li) => {
+  li.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
